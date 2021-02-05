06b7360a75859ce62d8bab010f09e00135e9ed15
yaodianmi <miyao.programmer@gmail.com>
2021-02-05 10:44:21 +0800

    修改配置，ip/静态文件等

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')*wqv(z8f_-h8_@6n0sx^dbkx&0_000bkvu^+(9^&*#dor6*2)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
