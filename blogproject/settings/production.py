951b505699bb7727faca83ffa09de430ec903d72
yaodianmi <miyao.programmer@gmail.com>
2021-02-07 11:05:51 +0800

    修改允许IP

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -2,4 +2,4 @@ from .common import *
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 DEBUG = True
-ALLOWED_HOSTS = ['82.156.109.20', 'hellodjango-blog-ydm.yaodianmi.top']
+ALLOWED_HOSTS = ['82.156.109.20', 'www.yaodianmi.top', 'blog.yaodianmi.top']
