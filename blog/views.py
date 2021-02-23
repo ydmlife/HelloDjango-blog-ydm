fc0776e08a3e4637c113eddbfda7c3a216c228e8
yaodianmi <miyao.programmer@gmail.com>
2021-02-23 17:31:29 +0800

    add PostDetailView

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ import re
 import markdown
 from markdown.extensions.toc import TocExtension
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from django.utils.text import slugify
 
@@ -15,24 +15,39 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
 
-    # 阅读量 +1
-    post.increase_views()
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
 
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',  # extra 本身包含很多基础拓展
-        'markdown.extensions.codehilite',  # codehilite 是语法高亮拓展
-        # 记得在顶部引入 TocExtension 和 slugify
-        TocExtension(slugify=slugify),
-    ])
-    post.body = md.convert(post.body)
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
 
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
-    
-    return render(request, 'blog/detail.html', context={'post': post})
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',  # extra 本身包含很多基础拓展
+            'markdown.extensions.codehilite',  # codehilite 是语法高亮拓展
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+        
+        return post
 
 class ArchiveView(IndexView):
     def get_queryset(self):
